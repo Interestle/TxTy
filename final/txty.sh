b39ed38eedfd52d148a7484d6879666c9abc1541
Colton <u0935511@utah.edu>
2021-12-07 22:31:26 +0000

    updated service

--- final/txty.sh
+++ final/txty.sh
@@ -5,7 +5,7 @@ SAVEFILE=/home/pi/TxTy/final/txtySave.txt
 # Load some settings if there aren't any.
 if [ ! -e "$SAVEFILE" ]; then
   touch /home/pi/TxTy/final/txtySave.txt
-  echo "0 0 42775 0 16 1 512" > /home/pi/TxTy/TxTy/final/txtySave.txt
+  echo "0 0 42775 0 16 1 512" > /home/pi/TxTy/final/txtySave.txt
 fi
 
 sudo /home/pi/TxTy/final/txty
